08be1ea269058a70342591aa9c71e12ab5832208
Aman-Bhar-Dev <amanbhar.aus@gmail.com>
2025-07-10 01:08:04 +0530

    Update for phone number and institute in admin models.py

--- conference/models.py
+++ conference/models.py
@@ -7,6 +7,18 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
+#------------------
+# for phonenumber and institute in admin panel
+#----------------------------
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone = models.CharField(max_length=15, blank=True)
+    institute = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
 
 # -------------------------------
 # AbstractSubmission & CoAuthor
