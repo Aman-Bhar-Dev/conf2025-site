e8480f0ffa0672ae1c892464005b00db4659a3a6
Aman Bhar <amanbhar.aus@gmail.com>
2025-07-23 00:02:26 +0530

    Update 0020_remove_finalregistration_has_paid.py

--- conference/migrations/0020_remove_finalregistration_has_paid.py
+++ conference/migrations/0020_remove_finalregistration_has_paid.py
@@ -5,7 +5,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("conference", "0008_finalregistration_selected_theme.py"),
+        ("conference", "0008_finalregistration_selected_theme"),
     ]
 
     operations = [
